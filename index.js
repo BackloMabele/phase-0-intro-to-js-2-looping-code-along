59d804756c39951792caf33808fd23d8d74f09d4
BackloMabele <backlo.mabele@student.moringaschool.com>
2023-09-21 12:36:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    const messages = [];        
+    for(let i=0; i < array.length; i++) {
+            const newArray = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+            messages.push(newArray);       
+        }
+    
+    return messages;
+}
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
